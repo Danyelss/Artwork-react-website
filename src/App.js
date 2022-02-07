9af6588718c8eb8e82ef8f1a09186ca3b8634da0
Dani Mos <danyelssmos99@gmail.com>
2022-02-07 09:02:33 +0200

    Router prototype added, needs changes after page creation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,48 @@
 import './App.css';
 import ArticlePage from './pages/Article';
+import HomePage from './pages/Homepage';
+import ErrorPage from './pages/Errorpage';
+import ArticlesPage from './pages/ArticlesPage';
+import ArtworksPage from './pages/ArtworksPage';
 import HeaderComponent from './components/header';
+
+import {
+  Routes,
+  Route,
+  BrowserRouter,
+  Navigate,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <HeaderComponent />
-      <ArticlePage />
-    </div>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+
+          <Route path="/404" element={<ErrorPage />} />
+          <Route path="*" element={<Navigate replace to="/404" />} />
+
+          <Route path="/option" element={<OptionPage />} />
+          <Route path="/articles" element={<ArticlesPage />} />
+          <Route path="/artworks" element={<ArtworksPage />} />
+
+          <Route
+            path="/upload"
+            element={
+              <PrivateRoute>
+                <UploadPage />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
   );
+}
+
+function PrivateRoute({ children }) {
+  //const auth = getAccessToken();
+
+  //return auth ? children : <Navigate to="/" />;
 }
 
 export default App;
