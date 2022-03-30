8c36cc37209ba26ac4e01e59ca16f2992794a614
Dani Mos <danyelssmos99@gmail.com>
2022-03-30 14:21:17 +0300

    header image

--- src/components/header.js
+++ src/components/header.js
@@ -2,12 +2,16 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Link } from 'react-router-dom';
 import '../App.css';
+import square from "../assets/square.png";
 
 const HeaderComponent = () => {
   const navigate = useNavigate();
   return (
-    <div className="header" onClick={() => navigate(-1)}> 
-    <h1>Back</h1>
+    <div onClick={() => navigate(-1)}>
+      <div className="headerParent">
+        <div className="headerArrow"></div>
+        <div className="headerSquare"></div>
+      </div>
     </div>
   );
 }
