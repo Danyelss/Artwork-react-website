29b1a167975ea0e46b95e23ee9a364aaf09d1d4d
Dani Mos <danyelssmos99@gmail.com>
2022-03-30 15:04:28 +0300

    header display modified

--- src/components/header.js
+++ src/components/header.js
@@ -3,14 +3,22 @@ import { useNavigate } from "react-router-dom";
 import { Link } from 'react-router-dom';
 import '../App.css';
 import square from "../assets/square.png";
+import arrow from "../assets/arrow.png";
 
 const HeaderComponent = () => {
   const navigate = useNavigate();
   return (
     <div onClick={() => navigate(-1)}>
       <div className="headerParent">
-        <div className="headerArrow"></div>
-        <div className="headerSquare"></div>
+        <img className="headerArrow" src={arrow}></img>
+        <img className="headerSquare" src={square}></img>
+        <img className="headerSquare" src={square}></img>
+        <img className="headerSquare" src={square}></img>
+        <img className="headerSquare" src={square}></img>
+        <img className="headerSquare" src={square}></img>
+        <img className="headerSquare" src={square}></img>
+        <img className="headerSquare" src={square}></img>
+        <img className="headerSquare" src={square}></img>
       </div>
     </div>
   );
